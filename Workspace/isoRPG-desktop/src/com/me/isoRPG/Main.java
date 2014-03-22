46d860770f4dc98f44c937d15549eb14c4b13f0e
BeanSeed <vovakster@gmail.com>
2014-03-22 14:45:14 -0700

    Added workspace
    Created our project files.

---
+++ Workspace/isoRPG-desktop/src/com/me/isoRPG/Main.java
@@ -0,0 +1,15 @@
+package com.me.isoRPG;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "isoRPG";
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new mainRPG(), cfg);
+	}
+}
